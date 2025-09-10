9a89c958f994c3bc45ebe14780b2ecb6360d6c39
Jigglyel <raphael.croix.albert@gmail.com>
2025-09-10 23:42:18 +0200

    partie 1 du TP FAIT

--- TPAlgo/TP1.cpp
+++ TPAlgo/TP1.cpp
@@ -1,6 +1,8 @@
- #include<iostream>
- #include<string>
-  #include<array>
+#include<iostream>
+#include<string>
+#include<array>
+#include<limits>
+#include<fstream>
 
 
  struct occMot
@@ -32,7 +34,6 @@
  void initialise(listeMots&L){
     L=nullptr;
  }
-
  void ajouteDebut (std::string mot,listeMots&L){
     maillon* ajout=new maillon;
     ajout->val.mot=mot;
@@ -45,20 +46,21 @@
     {
         ajouteDebut(mot,L);
     }
-    
-    if (L->val.mot==mot)
-    {
-        L->val.occ++;
-    }
-    if(L->val.mot<mot)
-    {
-        ajout(mot,L->suiv);
-    }
     else
     {
-        ajouteDebut(mot,L);
+        if (L->val.mot==mot)
+        {
+            L->val.occ++;
+        }
+        if(L->val.mot<mot)
+        {
+            ajout(mot,L->suiv);
+        }
+        if(L->val.mot>mot)
+        {
+            ajouteDebut(mot,L);
+        }
     }
-
     
  }
 void affiche(listeMots L){
@@ -70,7 +72,23 @@ void affiche(listeMots L){
     else
         std::cout<<std::endl;
 }
-int taille(listeMots L)
+void supprime(std::string mot, listeMots& L)
+{
+    if (L!=nullptr)
+    {
+        if (L->val.mot==mot)
+        {
+            maillon*p;
+            p=L;
+            L=L->suiv;
+            delete p;
+            supprime(mot,L);
+        }
+        supprime(mot,L->suiv);
+    }
+    
+}
+unsigned int taille(listeMots L)
 {
     maillon*q=L;
     unsigned int count=0;;
@@ -118,26 +136,185 @@ bool estValide(listeMots L)
 }
 int plusLong (listeMots L)
 {
-    int max=0;
+    unsigned int max=0;
     while(L!=nullptr)
     {
-        if(max<L->val.mot)
+        if(max<L->val.mot.length())
             max=L->val.mot.length();
+        L=L->suiv;
     }
     return max;
 }
-
-int main(){
-    listeMots L;
-    initialise(L);
-    ajouteDebut("saucisse",L);
-    ajouteDebut("Mario_Kart",L);
-    ajout("Nintendo!",L);
-    if (estValide(L))
+void afficheLongueur(listeMots L,unsigned int longueur)
+{
+     while(L!=nullptr)
     {
-        std::cout<<"fine"<<std::endl;
+        if(longueur==L->val.mot.length())
+            std::cout<<L->val.mot<<" ";
+        L=L->suiv;
+    }
+        std::cout<<std::endl;
+}
+void ajoutParLongueur(std::string mot,listeMots &L)
+{
+   if (L==nullptr)
+    {
+        ajouteDebut(mot,L);
+    }
+    else
+    {
+        if (L->val.mot==mot)
+        {
+            L->val.occ++;
+        }
+        if(L->val.mot.length()<mot.length())
+        {
+            ajoutParLongueur(mot,L->suiv);
+        }
+        else
+        {
+            ajouteDebut(mot,L);
+        }
     }
     
-    affiche(L);
+}
+void afficheTriLongueur(listeMots & L)
+{
+    maillon*q=L;
+    maillon*c=nullptr;
+    //copie de la liste L dans c grace au pointeur de maillon q tout en récupérant, affichant et supprimant le mot le plus petit
+    for (unsigned int i = 0; i < taille(L); i++)
+    {
+        ajoutParLongueur(q->val.mot,c);
+        q=q->suiv;
+    }
+    affiche(c);
+}
+void saisie (listeMots & L) {
+    std::string mot;
+    std::cout<<"quel mode voulez vous choisir? phrase ou liste?"<<std::endl;
+    std::cout<<"reponse :";
+    std::cin>>mot;
+    if (mot=="liste")
+    {
+        while (mot!="fini")
+        {
+            std::cout<<"saisissez votre mot ('fini' pour arreter) :";
+            std::cin>>mot;
+            if (mot=="fini")
+            {
+                std::cout<<"voulez vous finir ou sasir le mot 'fini'?";
+                std::cout<<"réponse(finir ou saisir) :";
+                std::cin>>mot;
+                if (mot!="finir")
+                {
+                    mot="fini";
+                    ajout(mot,L);
+                }
+                else
+                    mot="fini";
+            }
+            else
+                ajout(mot,L);
+        }
+        
+    }
+    else
+    {
+        mot="";
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::string aux;
+        std::string phrase;
+        std::cout<<"saisissez votre phrase:";
+        std::getline(std::cin,phrase);
+        aux=phrase;
+        for (unsigned int i = 0; i < phrase.length(); i++)
+        {
+            if (phrase[i]==' ')
+            {
+                ajout(mot,L);
+                mot="";
+            }
+            else
+            {
+                aux=aux[i];
+                mot.append(aux);
+            }
+        }
+        ajout(mot,L);
+    }
+    
+}
+void construit (listeMots & L, std::string nomFichier)
+{
+    std::ifstream fic;
+    std::string mot;
+    fic.open(nomFichier);
+    if (fic.is_open())
+        while (fic.good())
+        {
+            fic>>mot;
+            ajout(mot,L);
+        }
+}
+void remplit (tabMots & T, listeMots L)
+{
+    unsigned int tailleDeL=taille(L);
+    T.tab=new std::string[tailleDeL];
+    T.taille=0;
+    for (unsigned int i = 0; i < tailleDeL; i++)
+    {
+        T.tab[i]=L->val.mot;
+        T.taille++;
+        L=L->suiv;
+    }
+}
+void afficheTab (tabMots & T) 
+{
+    for (unsigned int i = 0; i < T.taille; i++)
+    {
+        std::cout<<T.tab[i]<<" ";
+    }
+    std::cout<<std::endl;
+}
+bool recherche_dichotomique(unsigned int a, unsigned int b,std::string mot,tabMots T){
+    unsigned int x=a+b;
+    x=x/2;
+    if (mot==T.tab[x])
+        return true;
+    else{
+        if (b-a<=1)
+            return false;
+        else{
+            if (mot<T.tab[x])
+            {
+                return recherche_dichotomique(a,x,mot,T);
+            }
+            else
+                return recherche_dichotomique(x,b,mot,T);
+        }
+    }
+    
+}
+bool appartient (std::string mot, tabMots T)
+{
+    return recherche_dichotomique(0,T.taille,mot,T);
+}
+int main(){
+    listeMots L;
+    tabMots T;
+    std::string mot;
+    initialise(L);
+    construit(L,"TP1.txt");
+    afficheTriLongueur(L);
+    remplit(T,L);
+    std::cout<<"saisir un mot : ";
+    std::cin>>mot;
+    if (appartient(mot,T))
+    {
+        std::cout<<"le mot appartient au texte"<<std::endl;
+    }
+    else
+        std::cout<<"le mot n'appartient pas au texte"<<std::endl;
     return 0;
 }
