432e94b3b297dfd896cfb9a5ed315d408a30da97
Croix Raphael <racroix@fixe-g101-07>
2025-09-24 10:53:18 +0200

    fin tp3

--- TPAlgo/TP3.cpp
+++ TPAlgo/TP3.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include<array>
 #include<cmath>
+#include<cctype>
 
 
 
@@ -15,7 +16,7 @@ using pile=maillon*;
 struct tab
 {
     std::array<std::string,100>tab;
-    int taille;
+    int taille=0;
 };
 float dépiler(pile &P)
 {
@@ -54,6 +55,51 @@ void saisie(tab&T)
     }
     
 }
+bool isdigit(char c)
+{
+    return (c=='1' or c=='2' or c=='3' or c=='4' or c=='5' or c=='6' or c=='7' or c=='8' or c=='9' or c=='0' or c=='.');
+}
+bool isnumber(std::string phrase)
+{
+    for(long unsigned int i=0;i<phrase.length();++i)
+        if(not isdigit(phrase[i]))
+            return false;
+    return true;
+}
+bool saisie2(tab&T)
+{
+    std::string expression;
+    int j=0;
+    std::cout<<"veuillez entrer votre formule :";
+    std::getline(std::cin,expression);
+    for(long unsigned int i=0;i<expression.length();++i)
+    {
+        if(expression[i]!=' ')
+        {
+            T.tab[j]+=expression[i];      
+        }
+        else
+        {
+            if(T.tab[j]!="PI" and T.tab[j]!="RAC" and T.tab[j]!="^" and T.tab[j]!="+" and T.tab[j]!="-" and T.tab[j]!="*" and T.tab[j]!="/")
+                if(not isnumber(T.tab[j]))
+                {
+                    return false;
+                }
+            ++j;
+            ++T.taille;
+        }
+    }
+    if(T.tab[j]!="PI" and T.tab[j]!="RAC" and T.tab[j]!="^" and T.tab[j]!="+" and T.tab[j]!="-" and T.tab[j]!="*" and T.tab[j]!="/")
+                if(not isnumber(T.tab[j]))
+                {
+                    std::cout<<"l'expression est incorrect ! "<<std::endl;
+                    return false;
+                }
+    ++T.taille;
+    return true;
+
+
+}
 float puissance(float val, float puiss)
 {
     if (puiss==0)
@@ -65,58 +111,77 @@ float puissance(float val, float puiss)
 }
 float évaluation(tab T)
 {
-    pile P;
+    pile P=nullptr;
+    int taille=0;
     float aux;
     for (int i = 0; i < T.taille; i++)
     {
+        //std::cout<<T.tab[i]<<std::endl;
         if (T.tab[i]=="+")
         {   aux=dépiler(P);
             empiler(P,dépiler(P)+aux);
+            --taille;
         }
         else
         if (T.tab[i]=="-")
         {
             aux=dépiler(P);
             empiler(P,dépiler(P)-aux);
+            --taille;
         }
         else
         if (T.tab[i]=="/")
         {
             aux=dépiler(P);
             empiler(P,dépiler(P)/aux);
+            --taille;
+
         }
         else
         if (T.tab[i]=="*")
         {
             aux=dépiler(P);
             empiler(P,dépiler(P)*aux);
+            --taille;
         }
+        else
         if (T.tab[i]=="^")
         {
             aux=dépiler(P);
             empiler(P,puissance(dépiler(P),aux));
+            --taille;
         }
+        else
         if (T.tab[i]=="RAC")
         {
             empiler(P,std::sqrt(dépiler(P)));
         }
 
         else
+        {
             if (T.tab[i]=="PI")
             {
                 empiler(P,3.14159);
             }
             else
-            std::cout<<P->val<<std::endl;
-                empiler(P,std::stof(T.tab[i])); 
+            {
+                empiler(P,std::stof(T.tab[i]));
+            }
+            ++taille;
+        }
     }
-    return dépiler(P);
+    if(taille==1)
+        return dépiler(P);
+    else{
+        std::cout<<"l'expression est incomplète"<<std::endl;
+        return -1;
+    }
 }
 
 
 int main()
 {
     tab T;
-    saisie(T);
-    std::cout<<"l'évaluation de la formule est : "<<évaluation(T)<<std::endl;
+    if(saisie2(T))
+        std::cout<<"l'évaluation de la formule est : "<<évaluation(T)<<std::endl;
 }
