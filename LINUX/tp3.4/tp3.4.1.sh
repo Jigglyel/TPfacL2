cafd2115a675d41509e387f2861d9e7c7ec14a1e
Croix Raphael <racroix@fixe-g101-03>
2026-02-06 11:45:32 +0100

    changement truc tp

--- LINUX/tp3.4/tp3.4.1.sh
+++ LINUX/tp3.4/tp3.4.1.sh
@@ -5,18 +5,36 @@ for  ligne in $(exiftool $1) ; do
 done
 
 
-
-
-mkdir galerie;
+mkdir galerie
+placed="First"
+mask=.*
+mask=$3
 for((i=0;i<${#fichiers[@]};++i)) ; do
-
+    
     if [[ ${fichiers[$i]} = "========" ]]  
     then
+        if [[ $placed = "False" && $2 = "precise" && $(echo $path|cut -d "/" -f2 ;) == $mask ]] ; then
+            echo "veuillez indiquer le date à utiliser, format YYYY:MM:DD"
+            read val
+            exiftool -overwrite_original -DateTimeOriginal="$(echo $val|cut -d ":" -f1 ;):$(echo $val|cut -d ":" -f2 ;):$(echo $val|cut -d ":" -f3 ;) 00:00:00" $path
+            mkdir galerie/$(echo $val|cut -d ":" -f1 ;)
+            mkdir galerie/$(echo $val|cut -d ":" -f1 ;)/$(echo $val|cut -d ":" -f2 ;)
+            mkdir galerie/$(echo $val|cut -d ":" -f1 ;)/$(echo $val|cut -d ":" -f2 ;)/$(echo $val|cut -d ":" -f3 ;)
+            echo "test"
+            mv $path galerie/$(echo $val|cut -d ":" -f1 ;)/$(echo $val|cut -d ":" -f2 ;)/$(echo $val|cut -d ":" -f3 ;)
+        fi
+
+         
         path=${fichiers[$i+1]}
+        placed="False"
     fi
-    if [[ ${fichiers[$i]} = "Date/Time" && ${fichiers[$i+1]} = "Original" ]] ; 
+    
+    if [[ ${fichiers[$i]} = "Date/Time" && ${fichiers[$i+1]} = "Original" && $(echo $path|cut -d "/" -f2 ;) == $mask ]]  
     then
-        mkdir galerie/$(cut -d ":" -f1 ${fichiers[$i+3]};)/$(cut -d ":" -f2 ${fichiers[$i+3]};)/$(cut -d ":" -f3 ${fichiers[$i+3]};)
-        mv $path galerie/$(cut -d ":" -f1 ${fichiers[$i+3]};)/$(cut -d ":" -f2 ${fichiers[$i+3]};)/$(cut -d ":" -f3 ${fichiers[$i+3]};)
+        mkdir galerie/$(echo ${fichiers[$i+3]}|cut -d ":" -f1 ;)
+        mkdir galerie/$(echo ${fichiers[$i+3]}|cut -d ":" -f1 ;)/$(echo ${fichiers[$i+3]}|cut -d ":" -f2 ;)
+        mkdir galerie/$(echo ${fichiers[$i+3]}|cut -d ":" -f1 ;)/$(echo ${fichiers[$i+3]}|cut -d ":" -f2 ;)/$(echo ${fichiers[$i+3]}|cut -d ":" -f3 ;)
+        mv $path galerie/$(echo ${fichiers[$i+3]}|cut -d ":" -f1 ;)/$(echo ${fichiers[$i+3]}|cut -d ":" -f2 ;)/$(echo ${fichiers[$i+3]}|cut -d ":" -f3 ;)
+        placed="True"
     fi
 done
